89a7e4e116a08c3bbceca8f2d533ab7b02e9d768
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-13 20:48:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# My Code here....
+map_to_no_change(source_array)
+  index = 0
+  new_array = []
+    while index < source_array.length do
+      new_array << source_array[index]
+      index += 1
+    end
+  new_array
+end
+
+
+
