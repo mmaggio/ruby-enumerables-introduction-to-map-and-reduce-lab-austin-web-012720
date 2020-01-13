12a5100ab656488a258810745aa92049acc0c283
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-13 20:50:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,15 @@
-map_to_no_change(source_array)
+def map_to_negativize(source_array)
+  index = 0
+  new_array = []
+    while index < source_array.length do
+      negative = source_array[index] * (-1)
+      new_array << negative
+      index += 1
+    end
+  new_array
+end
+
+def map_to_no_change(source_array)
   index = 0
   new_array = []
     while index < source_array.length do
@@ -8,5 +19,58 @@ map_to_no_change(source_array)
   new_array
 end
 
+def map_to_double(source_array)
+  index = 0
+  new_array = []
+    while index < source_array.length do
+      double = source_array[index] * 2
+      new_array << double
+      index += 1
+    end
+  new_array
+end
+
+def map_to_square(source_array)
+  index = 0
+  new_array = []
+    while index < source_array.length do
+      square = source_array[index] ** 2
+      new_array << square
+      index += 1
+    end
+  new_array
+end
+
+
+
+def reduce_to_total(source_array, starting_point, 0)
+  result = 0
+  index = starting_point
+  while starting_point < source_array.length do
+      result += source_array[starting_point]
+      starting_point += 1
+    end
+  result
+end
+
+def reduce_to_all_true(source_array)
+  index = 0
+  while index < source_array.length do
+      if source_array[index]
+        return true
+      end
+    index += 1
+  end
+end
+
+def reduce_to_any_true(source_array)
+  index = 0
+    while index < source_array.length do
+      if source_array[index]
+        return source_array[index]
+      end
+     index += 1
+    end
+end
 
 
