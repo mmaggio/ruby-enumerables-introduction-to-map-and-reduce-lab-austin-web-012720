bbed6cda2ecb78bfd0ce8ba491e4f1c0afef5b16
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-13 21:05:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,13 +68,15 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  index = 0
-    while index < source_array.length do
+ index = 0
+  while index < source_array.length do
       if source_array[index]
-        return source_array[index]
+        answer = !!5
+      else
+        answer = !!nil
       end
-     index += 1
-    end
+      index += 1
+  end
+  return answer
 end
-
 
